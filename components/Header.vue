fad2eaadfceb281c3fdd342111a567588f5dad07
Suguru Ohki <oki.work0527@gmail.com>
2019-11-05 23:55:16 +0900

    Add Header and Footer

---
+++ components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Header</h1>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
+    <Header/>
     <nuxt/>
+    <Footer/>
   </div>
 </template>
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
 
 <style lang="scss">
 $global-font: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
   Roboto, 'Helvetica Neue', Arial, sans-serif;
 
 $default-font-size: 16px;
+$default-font-color: #ffffff;
 
 html {
+  color: $default-font-color;
   font-family: $global-font;
   font-size: $default-font-size;
   word-spacing: 1px;
@@ -19,41 +33,8 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  body {
+    background-color: #0079bf;
+  }
 }
 </style>
