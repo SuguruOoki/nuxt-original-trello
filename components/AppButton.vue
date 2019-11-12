a0497354779cbbfe2a1e634cf4a2134666d4f1ab
suguru ohki <suguruohki@sugurunoMacBook-Pro.local>
2019-11-12 23:32:46 +0900

    Add AppButton component

---
+++ components/AppButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="button" :style="styles">{{ buttonText }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      require: true
+    },
+    backgroundColor: {
+      type: String,
+      require: false,
+      default: 'red'
+    },
+    width: {
+      type: String,
+      require: false,
+      default: '100%'
+    }
+  },
+  computed: {
+    styles() {
+      return {
+        'background-color': this.backgroundColor,
+        'width': this.width
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  background-color: #ffffff;
+  color: #172b4d;
+  border-radius: 4px;
+  padding: 12px 16px;
+}
+</style>
